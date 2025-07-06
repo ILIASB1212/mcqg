5d748c68fb29ac29564b0b410677f136a3eed2de
ILIASB1212 <iliasbessiness123456@gmail.com>
2025-07-06 15:54:40 +0100

    end 1

--- src/mcqgenerator/StreamLitApp.py
+++ src/mcqgenerator/StreamLitApp.py
@@ -16,5 +16,57 @@ from MCQGenerator import generate_evaluate_chain
 
 
 
+
 with open(r"C:\Windows\System32\mcqg\responce.json","r") as f:
-    response_json=json.loads(f)
+    content = f.read()
+    response_json = json.loads(content)
+
+
+
+st.title("MCQ GENERATOR APPLICATION WITH LANGCHAIN")
+
+with st.form("user input"):
+    file=st.file_uploader("upload file")
+    number=st.number_input("nuber of question",min_value=3,max_value=25)
+    tone=st.text_input("select the deficulty of mcqor parametre",placeholder="SIMPLE",max_chars=25 )
+    subject=st.text_input("select the subject",placeholder="history/math......",max_chars=25 )
+    button=st.form_submit_button("create mcq")
+    if button and file is not None and number and tone and subject:
+        with st.spinner("generating...."):
+            try:
+                
+                text=read_file(file)
+                with get_openai_callback() as cb:
+                    response=generate_evaluate_chain(
+                                {"text":text,
+                                "number":number,
+                                "subject":subject,
+                                "tone":tone,
+                                "response_json":json.dumps(response_json)
+                                }
+                            )
+            except Exception as e:
+                traceback.print_exception(type(e),e,e.__traceback__)
+                st.error("error")
+            else:
+                print(f"Total Tokens: {cb.total_tokens}")
+                print(f"Prompt Tokens: {cb.prompt_tokens}")
+                print(f"Completion Tokens: {cb.completion_tokens}")
+                print(f"Total Cost: {cb.total_cost}")
+
+                if isinstance(response, dict):
+                    # Extract the quiz data from the response
+                    quiz = response.get("quiz", None)
+                    
+                    if quiz is not None:
+                        table_data = get_table_data(quiz)
+                        
+                        if table_data is not None:
+                            df = pd.DataFrame(table_data)
+                            df.index = df.index + 1
+                            st.table(df)
+                            st.text_area(label="Review",value=response["review"])
+                        else:
+                            st.error("error in the table data")
+                    else:
+                        st.write(response) 
---
+++ src/mcqgenerator/utils.py
@@ -0,0 +1,55 @@
+import PyPDF2
+import os
+import traceback
+import json
+
+
+
+def read_file(file):
+    filename = file.name.lower()
+
+    if filename.endswith(".pdf"):
+        try:
+            reader = PyPDF2.PdfReader(file)
+            full_text = []
+            for page in reader.pages:
+                text = page.extract_text()
+                full_text.append(text)
+            return "\n".join(full_text)
+        except Exception as e:
+            raise Exception(f"Error reading PDF: {e}")
+
+    elif filename.endswith(".txt"):
+        try:
+            return file.read().decode("utf-8")
+        except Exception as e:
+            raise Exception(f"Error reading TXT file: {e}")
+
+    else:
+        raise Exception("Unsupported file format. Only PDF and TXT are supported.")
+
+    
+def get_table_data(quiz_str):
+        try:
+            # convert the quiz from a str to dict
+            quiz_dict=json.loads(quiz_str)
+            quiz_table_data=[]
+            
+            # iterate over the quiz dictionary and extract the required information
+            for key,value in quiz_dict.items():
+                mcq=value["mcq"]
+                options=" || ".join(
+                    [
+                        f"{option}-> {option_value}" for option, option_value in value["options"].items()
+                    
+                    ]
+                )
+                
+                correct=value["correct"]
+                quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
+            
+            return quiz_table_data
+            
+        except Exception as e:
+            traceback.print_exception(type(e), e, e.__traceback__)
+            return False
